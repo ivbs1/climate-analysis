a9b1fdcacbd13a839d43a12f6ebfb053cb34e601
ivbs1 <ivbs1u22@soton.ac.uk>
2022-11-15 10:35:51 +0000

    Add Docstring

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,3 +1,5 @@
+""" Climate Analysis Tools """
+
 import sys
 import temp_conversion
 import signal
@@ -25,3 +27,4 @@ for line in climate_data:
             kelvin = temp_conversion.fahr_to_kelvin(fahr)
 
             print(str(celsius)+", "+str(kelvin))
+# TODO(js-robinson): Add call to process rainfall
